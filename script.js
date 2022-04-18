59433152f45a44608aba9bb4b1c60bc76b28bdb9
Skraevern <86933999+Skraevern@users.noreply.github.com>
2022-04-18 15:21:04 +0200

    Added functions for color buttons

--- script.js
+++ script.js
@@ -32,9 +32,39 @@ let grid64x64 = function () {
   }
 };
 
-grid16x16();
+let markFunction = function () {};
 
-paper.addEventListener(`mouseover`, (e) => e.target.classList.add(`active`));
+let blackColor = function () {
+  paper.addEventListener(
+    `mouseover`,
+    (e) => (e.target.style.backgroundColor = "black")
+  );
+};
+let redColor = function () {
+  paper.addEventListener(
+    `mouseover`,
+    (e) => (e.target.style.backgroundColor = "red")
+  );
+};
+let greenColor = function () {
+  paper.addEventListener(
+    `mouseover`,
+    (e) => (e.target.style.backgroundColor = "green")
+  );
+};
+let blueColor = function () {
+  paper.addEventListener(
+    `mouseover`,
+    (e) => (e.target.style.backgroundColor = "blue")
+  );
+};
+grid16x16();
+let pixel = document.getElementById("div-16x16");
+blackColor();
 
 btn16x16.addEventListener(`click`, grid16x16);
 btn64x64.addEventListener(`click`, grid64x64);
+blackBtn.addEventListener(`click`, blackColor);
+redBtn.addEventListener(`click`, redColor);
+greenBtn.addEventListener(`click`, greenColor);
+blueBtn.addEventListener(`click`, blueColor);
